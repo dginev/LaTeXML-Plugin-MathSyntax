3c6a242ce9e0e672e316342f46f151752b56ad0a
Deyan Ginev <d.ginev@jacobs-university.de>
2013-09-03 04:07:32 -0400

    a basic implementation for cmml to xmath weakening

--- lib/LaTeXML/Util/TestMath.pm
+++ lib/LaTeXML/Util/TestMath.pm
@@ -14,6 +14,8 @@ package LaTeXML::Util::TestMath;
 use strict;
 use warnings;
 use Data::Dumper;
+use Scalar::Util qw/blessed/;
+use List::MoreUtils qw/natatime/;
 
 use LaTeXML::Converter;
 use LaTeXML::Util::Config;
@@ -21,10 +23,30 @@ use LaTeXML::Util::Config;
 use Test::More;
 use LaTeXML::Util::Test;
 our @ISA = qw(Exporter);
-our @EXPORT = (qw(anno_string_to_array
-  weaken_cmml_array cmml_to_xmath_array
-  parse_TeX),
+our @EXPORT = (qw(math_tests anno_string_to_array weaken_cmml parse_TeX),
   @Test::More::EXPORT);
+
+### Test API
+
+sub math_tests {
+  my (%options) = @_;
+  $options{tests} = [] unless defined $options{tests};
+  my $iterator = natatime 2, @{$options{tests}};
+
+  while (my ($input,$output) = $iterator->()) {
+    my $copy = $input;
+    my $array_expected = anno_string_to_array($output);
+    $array_expected = weaken_cmml($array_expected,$options{type});
+
+    my $xml_parse = parse_TeX($input,parser=>'LaTeXML::MathSyntax');
+    my $array_parse = xmldom_to_array($xml_parse);
+    # Unwrap the leading math/xmath if present
+    while ($array_parse->[0] =~ /^ltx:X?Math$/) {
+      $array_parse = $array_parse->[2]; }
+    is_deeply($array_parse, $array_expected, "Formula: $input");
+  }
+  done_testing();
+}
 
 ### Output/annotation manipulation
 # Marpa::R2 grammar converting an annotation string into a Perl array
@@ -41,7 +63,7 @@ Expression ::=
   | Term
 
 Application ::=
-  '@' '(' Expression Arguments')' action => apply
+  '(' Expression Arguments ')' action => apply assoc => group
 
 Arguments ::= 
   Expression action => merge
@@ -50,37 +72,69 @@ Arguments ::=
 Term ::= 
   Word ':' Word ':' Word action => csymbol
   || Word ':' Word action => basic_symbol
-  # Special case where the lexeme is a special char @:()
+  # Special case where the lexeme is a special char :()
   || ':' ':' Word ':' Word action => csymbol
-  | '@' ':' Word ':' Word action => csymbol
   | '(' ':' Word ':' Word action => csymbol
   | ')' ':' Word ':' Word action => csymbol
 
-Word ~ [^\s:@\(\)]+
+Word ~ [^\s\:\(\)]+
 :discard ~ whitespace
 whitespace ~ [\s]+
 
 END_OF_RULES
 });
 
+### Sub convenience routines
 sub anno_string_to_array {
   my ($annotation_string) = @_;
   my $recce = Marpa::R2::Scanless::R->new( { grammar => $string_to_array_grammar } );
   $recce->read( \$annotation_string );
   my $value_ref = $recce->value;
   my $value = $value_ref ? ${$value_ref} : 'No Parse';
-  # print STDERR Dumper($value);
+
   return $value; }
 
-sub weaken_cmml_array {
-  #TODO
-}
+sub weaken_cmml {
+  my ($cmml_array,$type) = @_;
+  return $cmml_array if (!$type || ($type eq 'semantic'));
+  # For now just downgrade to XMath immediately
+  return weaken_cmml_to_xmath($cmml_array); }
+
+our $xmath_name = {'apply'=>'ltx:XMApp','cn'=>'ltx:XMTok','ci'=>'ltx:XMTok','csymbol'=>'ltx:XMTok'};
+sub weaken_cmml_to_xmath {
+  my ($array) = @_;
+  # For now, simple renaming would do:
+  return $array unless ref $array;
+  my @copy = @$array;
+  my $content_head = shift @copy;
+  my $head = $xmath_name->{$content_head};
+  my $attributes = shift @copy;
+  $attributes->{omdcd} = delete $attributes->{cd} if exists $attributes->{cd};
+  my @body;
+  if ($content_head eq 'csymbol') {
+    @body = (delete $attributes->{lexeme});
+    $attributes->{meaning} = shift @copy; }
+  else {
+    @body = map {weaken_cmml_to_xmath($_)} @copy; }
+  
+  return [$head, $attributes, @body]; }
+
+sub xmldom_to_array {
+  my ($tree) = @_;
+  my $class = blessed($tree);
+  if ($class && ($class ne 'XML::LibXML::Text')) {
+    # XML leaf, turn to array:
+    my @attributelist = grep {defined} $tree->attributes();
+    my $attributes = {map {$_->localname() => $_->value()} @attributelist};
+    ['ltx:'.$tree->localname,$attributes,(grep {defined} map{xmldom_to_array($_)} $tree->childNodes)]; }
+  else {
+    (ref $tree) ? $tree->toString : $tree; }}
 
 ### Semantics
 sub CMML_Semantics::new {return {}; }
 
 sub CMML_Semantics::apply {
-  my (undef, $at, $open, $expr, $arguments, $close ) = @_;
+  my (undef, $open, $expr, $arguments, $close ) = @_;
   return ['apply',{},$expr,@$arguments]; }
 
 sub CMML_Semantics::merge {
@@ -101,15 +155,15 @@ sub CMML_Semantics::basic_symbol {
 ### Input manipulation
 
 sub parse_TeX {
-  my ($tex_math,$parser) = @_;
-  $parser //= 'LaTeXML::MathSyntax';
+  my ($tex_math,%options) = @_;
+  $options{parser} //= 'LaTeXML::MathSyntax';
   my $opts = LaTeXML::Util::Config->new(
     input_limit=>100,
     whatsin=>'math',
     whatsout=>'math',
     post=>0,
     verbosity=>2,
-    mathparse=>$parser,
+    mathparse=>$options{parser},
     defaultresources => 0,
     format=>'dom',
     preload=>[
